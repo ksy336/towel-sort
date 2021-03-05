208af71bc5637468b3eeb9eec298cac35f1ab5a5
Kseniya Klimova <ksuoxy773@gmail.com>
2021-03-05 23:11:49 +0400

    feat: add spliution to towl-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (arguments.length === 0 || matrix.length === 0) {
+        return [];
+    }
+    for (let j = 0; j < matrix.length; j++) {
+            if (j % 2 === 1) {
+                matrix[j].reverse();
+            }
+
+        }
+        let arr = [].concat(...matrix);
+        return arr;
+    }
+
+
+
+
+
